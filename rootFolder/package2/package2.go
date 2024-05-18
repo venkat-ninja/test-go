02032f99e074e89d75a2e83c0173745828f28f52
venkat-ninja <103577673+venkat-ninja@users.noreply.github.com>
2024-05-17 21:17:59 -0400

    Update package2.go

--- rootFolder/package2/package2.go
+++ rootFolder/package2/package2.go
@@ -1,7 +1,13 @@
 package package2
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func Printpack2() {
 	fmt.Print("Hello from package 2")
+	cmd_apikey := exec.Command("yq", "--version")
+	apikeyOut, _ := cmd_apikey.CombinedOutput()
+	fmt.Println("running apikey mask command :", string(apikeyOut))
 }
